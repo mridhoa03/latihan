9b99bf73e97e9370c5e3ff76ce491451cf9fc1c9
mridhoa03 <mridhoa03@gmail.com>
2019-07-06 09:55:37 +0300

    memperbaiki adam

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,16 +11,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>24 News — Free Website Template, Free HTML5 Template by FreeHTML5.co</title>
-    <link href="{{ asset('assets/frontend/css/media_query.css" rel="stylesheet" type="text/css')}}"/>
-    <link href="{{ asset('assets/frontend/css/bootstrap.css" rel="stylesheet" type="text/css')}}"/>
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+    <link href="{{ asset('assets/frontend/css/media_query.css')}}" rel="stylesheet" type="text/css')}}"/>
+    <link href="{{ asset('assets/frontend/css/bootstrap.css')}}" rel="stylesheet" type="text/css')}}"/>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css')}}" rel="stylesheet"
           integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link href="{{ asset('assets/frontend/css/animate.css" rel="stylesheet" type="text/css')}}"/>
+    <link href="{{ asset('assets/frontend/css/animate.css')}}" rel="stylesheet" type="text/css')}}"/>
     <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
-    <link href="{{ asset('assets/frontend/css/owl.carousel.css" rel="stylesheet" type="text/css')}}"/>
-    <link href="{{ asset('assets/frontend/css/owl.theme.default.css" rel="stylesheet" type="text/css')}}"/>
+    <link href="{{ asset('assets/frontend/css/owl.carousel.css')}}" rel="stylesheet" type="text/css')}}"/>
+    <link href="{{ asset('assets/frontend/css/owl.theme.default.css')}}" rel="stylesheet" type="text/css')}}"/>
     <!-- Bootstrap CSS -->
-    <link href="{{ asset('assets/frontend/css/style_1.css" rel="stylesheet" type="text/css')}}"/>
+    <link href="{{ asset('assets/frontend/css/style_1.css')}}" rel="stylesheet" type="text/css')}}"/>
     <!-- Modernizr JS -->
     <script src="{{ asset('assets/frontend/js/modernizr-3.5.0.min.js')}}"></script>
 </head>
@@ -638,10 +638,10 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js')}}"></script>
 <script src="{{ asset('assets/frontend/js/owl.carousel.min.js')}}"></script>
 <!--<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js')}}" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js')}}"
+<script src="{{ asset('assets/frontend/https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js')}}"
         integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
         crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js')}}"
+<script src="{{ asset('assets/fronted/https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js')}}"
         integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
         crossorigin="anonymous"></script>
 <!-- Waypoints -->
