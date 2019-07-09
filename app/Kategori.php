20de074962351b8f03029a692ef170be0565a561
mridhoa03 <mridhoa03@gmail.com>
2019-07-09 18:05:10 +0300

    anyarr

--- app/Kategori.php
+++ app/Kategori.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kategori extends Model
 {
-    protected $table = "kategoris";
+
     protected $fillable =  ['nama_kategori','slug'];
     public $timestamps = true;
 
